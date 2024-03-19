997cbaa7c2335fc39a537436610c673b6817ff50
leandro <leandrocmsantos@gmail.com>
2024-03-19 19:21:46 -0300

    push

--- sg.tf
+++ sg.tf
@@ -1,77 +1,77 @@
 
-resource "aws_security_group" "sg-rds" {
-  name        = "SG-${var.projectName}-rds"
-  description = var.projectName
-  vpc_id      = var.vpcId
+# resource "aws_security_group" "sg-rds" {
+#   name        = "SG-${var.projectName}-rds"
+#   description = var.projectName
+#   vpc_id      = var.vpcId
 
-  ingress {
-    description = "VPC"
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = ["${var.vpcCIDR}"]
-  }
+#   ingress {
+#     description = "VPC"
+#     from_port   = 5432
+#     to_port     = 5432
+#     protocol    = "tcp"
+#     cidr_blocks = ["${var.vpcCIDR}"]
+#   }
 
-  egress {
-    description = "VPC"
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = ["${var.vpcCIDR}"]
-  }
+#   egress {
+#     description = "VPC"
+#     from_port   = 5432
+#     to_port     = 5432
+#     protocol    = "tcp"
+#     cidr_blocks = ["${var.vpcCIDR}"]
+#   }
 
-  #ALL --------------------------------
-  ingress {
-    description = "VPC"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "tcp"
-    cidr_blocks = ["${var.vpcCIDR}"]
-  }
+#   #ALL --------------------------------
+#   ingress {
+#     description = "VPC"
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "tcp"
+#     cidr_blocks = ["${var.vpcCIDR}"]
+#   }
 
-  egress {
-    description = "VPC"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["${var.vpcCIDR}"]
-  }
+#   egress {
+#     description = "VPC"
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["${var.vpcCIDR}"]
+#   }
 
- #PORT --------------------------------
+#  #PORT --------------------------------
 
-  ingress {
-    description = "VPC"
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    ipv6_cidr_blocks = ["::/0"]
-  }
+#   ingress {
+#     description = "VPC"
+#     from_port   = 5432
+#     to_port     = 5432
+#     protocol    = "tcp"
+#     ipv6_cidr_blocks = ["::/0"]
+#   }
 
-  egress {
-    description = "VPC"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    ipv6_cidr_blocks = ["::/0"]
-  }
+#   egress {
+#     description = "VPC"
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     ipv6_cidr_blocks = ["::/0"]
+#   }
   
 
-#PORT --------------------------------
+# #PORT --------------------------------
 
-  ingress {
-    description = "VPC"
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     description = "VPC"
+#     from_port   = 5432
+#     to_port     = 5432
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  egress {
-    description = "VPC"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   egress {
+#     description = "VPC"
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-}
+# }
